262a740a4b28ed84318522657eb13a1461eb32e1
Christopher Mbaike <christophermbaike@gmail.com>
2024-03-17 16:07:18 +0000

    change plugin to calico

--- centos/vagrant/create-cluster.sh
+++ centos/vagrant/create-cluster.sh
@@ -8,7 +8,7 @@ then
   kubeadm init --pod-network-cidr $POD_CIDR --service-cidr $SERVICE_CIDR --apiserver-advertise-address $INTERNAL_IP
 
   kubectl --kubeconfig /etc/kubernetes/admin.conf \
-    apply -f "https://github.com/weaveworks/weave/releases/download/v2.8.1/weave-daemonset-k8s-1.11.yaml"
+    apply -f "kubectl create -f https://raw.githubusercontent.com/projectcalico/calico/v3.27.2/manifests/tigera-operator.yaml"
 
   mkdir -p /home/vagrant/.kube
   cp -i /etc/kubernetes/admin.conf /home/vagrant/.kube/config
