d83f9e93cfe3f107591e7c24f96c592a62680fa4
Christopher Mbaike <christophermbaike@gmail.com>
2024-03-17 16:31:11 +0000

    added custom resource for calico

--- centos/vagrant/create-cluster.sh
+++ centos/vagrant/create-cluster.sh
@@ -2,13 +2,15 @@ set -ex
 echo "Instance $HOSTNAME"
 if [[ "$HOSTNAME" = *controller* ]]
 then
-  POD_CIDR=10.244.0.0/16
-  SERVICE_CIDR=10.96.0.0/16
+  POD_CIDR=192.168.0.0/16
 
   kubeadm init --pod-network-cidr $POD_CIDR --service-cidr $SERVICE_CIDR --apiserver-advertise-address $INTERNAL_IP
 
   kubectl --kubeconfig /etc/kubernetes/admin.conf \
-    apply -f "kubectl create -f https://raw.githubusercontent.com/projectcalico/calico/v3.27.2/manifests/tigera-operator.yaml"
+    apply -f "https://raw.githubusercontent.com/projectcalico/calico/v3.27.2/manifests/tigera-operator.yaml"
+
+  kubectl --kubeconfig /etc/kubernetes/admin.conf \
+    apply -f "https://raw.githubusercontent.com/projectcalico/calico/v3.27.2/manifests/custom-resources.yaml"
 
   mkdir -p /home/vagrant/.kube
   cp -i /etc/kubernetes/admin.conf /home/vagrant/.kube/config
